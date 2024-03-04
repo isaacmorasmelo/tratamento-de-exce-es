7daac04cb54e4249202e93bdfcc8e0f8b420c88b
Isaac Moraes de Melo <isaacmoraesdemelo@gmail.com>
2024-03-04 10:22:55 -0400

    exercicio

---
+++ src/entities/Account.java
@@ -0,0 +1,55 @@
+package entities;
+
+public class Account {
+    private int number;
+    private String holder;
+    private double balance;
+    private double withdrawnLimit;
+
+    public void deposit(double amount){
+        balance += amount;
+    }
+
+    public void withdrawn(double amount){
+        balance -= amount;
+    }
+
+    public Account(int number, String holder, double balance, double withdrawnLimit) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.withdrawnLimit = withdrawnLimit;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getWithdrawnLimit() {
+        return withdrawnLimit;
+    }
+
+    public void setWithdrawnLimit(double withdrawnLimit) {
+        this.withdrawnLimit = withdrawnLimit;
+    }
+}
---
+++ src/fixacao.java
@@ -0,0 +1,13 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class fixacao {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data");
+
+
+    }
+}
